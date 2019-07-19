5edbf92519b0cb9c2cd9995198eb6074ed6145a0
Som Ramnani <som.ramnani@gmail.com>
2019-07-19 15:07:23 -0500

    added JawsDB

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var burger = require("../models/burger");
+var burger = require("../models/burger.js");
 var router = express.Router();
 
-router.get('/', function(req, res){
+// Get all the data
+router.get("/", function(req, res){
   burger.all(function(data){
     var hbObj = {
-      burgers: res
-    }
+      burgers: data
+    };
     res.render("index", hbObj);
-  })
+  });
 });
 
 router.post("/api/burgers", function(req, res){
@@ -18,8 +19,19 @@ router.post("/api/burgers", function(req, res){
   ], [
     req.body.burger_name, req.body.devoured
   ], 
-  function(result){
-    res.end();
+  function(data){
+    res.redirect('/')
+  })
+})
+
+
+
+router.put("/api/burgers/update/:id", function(req,res){
+  burger.update({
+    devoured: true
+
+  }, condition, function(data){
+    res.redirect('/')
   })
 })
 
@@ -30,17 +42,6 @@ router.delete('/api/burgers/delete/:id', function(req,res){
   function(){
     res.end();
   })
-})
-
-router.update("/api/burgers/update/:id", function(req,res,next){
-  burger.update([
-    req.body, req.params
-  ],[
-    function(){
-      res.end();
-    }
-  ])
-  
 })
 
 module.exports = router;
